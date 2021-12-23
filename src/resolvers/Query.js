d10e52ad96d32ac351d1d8c90627de518e93dd32
JosephGabriel <josegabriel.freelancer@gmail.com>
2021-12-23 14:17:21 -0300

    Query de hoteis concluida

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,13 +1,25 @@
 export const Query = {
+  //Retorna um usúarios usando o ID como parâmetro
+  async user(parent, { id }, { prisma }, info) {
+    const users = await prisma.query.users({ where: { id } }, info);
+    return users;
+  },
+
   //Retorna todos os usúarios
   async users(parent, args, { prisma }, info) {
     const users = await prisma.query.users(args, info);
     return users;
   },
 
-  //Retorna um usúarios usando o ID como parâmetro
-  async user(parent, { id }, { prisma }, info) {
-    const users = await prisma.query.users({ where: id }, info);
-    return users;
+  //Retorna um usúario usando o ID como parâmetro
+  async hotel(parent, { id }, { prisma }, info) {
+    const hotel = await prisma.query.hotel({ where: { id } }, info);
+    return hotel;
+  },
+
+  //Retorna todos os hóteis
+  async hotels(parent, args, { prisma }, info) {
+    const hotels = await prisma.query.hotels(args, info);
+    return hotels;
   },
 };
